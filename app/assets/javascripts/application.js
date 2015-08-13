85312f65cb4f3c37f9d7ab465347809980a32225
Trinh Tien Dat <dattt@asiantech.vn>
2015-08-13 10:55:54 +0700

    edit js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,12 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require_tree
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap.min
+//= require jquery.min
+
+
+
+//= require sb-admin-2
+
+//= require_tree .
